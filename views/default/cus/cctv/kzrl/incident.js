435c2ad416a3478fc6f5b6a28599e7b07f688b46
Jason Young <jasony62@qq.com>
2015-08-22 09:49:12 +0800

    更新抗战日历的日志记录

--- views/default/cus/cctv/kzrl/incident.js
+++ views/default/cus/cctv/kzrl/incident.js
@@ -4,13 +4,16 @@ if (/MicroMessenger/i.test(navigator.userAgent)) {
     wx.config(signPackage);
 }
 var app = angular.module('kzrl', []);
-app.config(['$locationProvider', function ($lp) {
+app.config(['$locationProvider', function($lp) {
     $lp.html5Mode(true);
 }]);
-app.controller('ctrl', ['$scope', '$http', '$location', '$sce', function ($scope, $http, $location, $sce) {
-    var id = $location.search().id, url = '/rest/cus/cctv/kzrl/get?articleid=' + id, debug = $location.search().debug;
+app.controller('ctrl', ['$scope', '$http', '$location', '$sce', function($scope, $http, $location, $sce) {
+    var id = $location.search().id,
+        url = '/rest/cus/cctv/kzrl/get?articleid=' + id,
+        debug = $location.search().debug;
     window.xxt.share.options.descAsTitle = true;
-    window.xxt.share.options.logger = function (shareto) {
+    //window.xxt.share.options.descAsTitle = false;
+    window.xxt.share.options.logger = function(shareto) {
         var url = "/rest/mi/matter/logShare";
         url += "?shareid=" + (new Date()).getTime();
         url += "&mpid=ad483481fb907d53d74130cd88e11d86";
@@ -20,7 +23,7 @@ app.controller('ctrl', ['$scope', '$http', '$location', '$sce', function ($scope
         url += "&shareto=" + shareto;
         $http.get(url);
     };
-    var setContent = function (data) {
+    var setContent = function(data) {
         var title;
         $scope.passed = Math.round((new Date().getTime() / 1000 - data.occured_time) / 86400);
         title = $scope.passed + '天前,' + data.title;
@@ -28,29 +31,36 @@ app.controller('ctrl', ['$scope', '$http', '$location', '$sce', function ($scope
         data.body = $sce.trustAsHtml(data.body);
         $scope.incident = data;
         window.xxt.share.set(title, location.href, data.summary, data.pic);
+        //window.xxt.share.set('70年前的8月15日 我们迎来日本宣布无条件投降', location.href, data.summary, data.pic);
     };
-    window.kzrlGeo.onLocationReady = function (lat, lng) {
+    window.kzrlGeo.onLocationReady = function(lat, lng) {
         if (lat !== undefined && lng !== undefined) {
             url += '&lat=' + lat;
             url += '&lng=' + lng;
         }
-        $http.get(url).success(function (rsp) {
-            if (angular.isString(rsp)) { alert(rsp); return; }
-            if (rsp.err_code !== 0) { alert(rsp.err_msg); return; }
+        $http.get(url).success(function(rsp) {
+            if (angular.isString(rsp)) {
+                alert(rsp);
+                return;
+            }
+            if (rsp.err_code !== 0) {
+                alert(rsp.err_msg);
+                return;
+            }
             setContent(rsp.data);
         });
     };
-    window.kzrlGeo.onLocationFail = function ($msg) {
-        $http.get(url).success(function (rsp) {
+    window.kzrlGeo.onLocationFail = function($msg) {
+        $http.get(url).success(function(rsp) {
             setContent(rsp.data);
         });
     };
-    $scope.openNearby = function () {
+    $scope.openNearby = function() {
         location.href = '/views/default/cus/cctv/kzrl/nearby.html?id=' + id;
     };
     $scope.isAssist = false;
-    $scope.clickAssist = function () {
-        $http.get('/rest/cus/cctv/kzrl/score?id=1577').success(function (rsp) {
+    $scope.clickAssist = function() {
+        $http.get('/rest/cus/cctv/kzrl/score?id=1577').success(function(rsp) {
             $scope.isAssist = true;
             $scope.assistCount = rsp.data[0];
         });
