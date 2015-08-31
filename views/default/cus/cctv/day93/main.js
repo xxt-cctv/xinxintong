f57a53051ec646ff4d81d03edbc31b7ee4672486
jasony62 <jasony62@qq.com>
2015-08-31 15:19:00 +0800

    code update

--- views/default/cus/cctv/day93/main.js
+++ views/default/cus/cctv/day93/main.js
@@ -1,14 +1,14 @@
 formApp = angular.module('formApp', []);
 formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.mpid = '9f4335dc25ab0a83c04e066793cba286';
-    $http.get('http://180.153.55.67/rest/cus/cctv/kzrl/byChannel?mpid=9f4335dc25ab0a83c04e066793cba286&id=15&page=1&size=2').success(function(rsp) {
+    $http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/byChannel?mpid=9f4335dc25ab0a83c04e066793cba286&id=15&page=1&size=2').success(function(rsp) {
         $scope.matters = rsp.data;
     });
 }]);
 (function() {
-    $('<img/>').attr('src', 'http://180.153.55.67/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
+    $('<img/>').attr('src', 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
         $(this).remove();
-        $('body').css('background-image', 'url(http://180.153.55.67/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
+        $('body').css('background-image', 'url(http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
     });
     var eSlider, oSlider, lastY;
     eSlider = document.querySelector('#slider');
--- views/default/cus/cctv/my93/page.js
+++ views/default/cus/cctv/my93/page.js
@@ -8,14 +8,14 @@ formApp.controller('formCtrl', ['$scope', '$http', '$timeout', '$q', function($s
     var r, submitWhole;
     submitWhole = function() {
         var url, d, d2, posted = angular.copy($scope.data);
-        url = 'http://180.153.55.67/rest/app/enroll/record/submit?mpid=' + $scope.mpid + '&aid=' + $scope.aid + '&submitkey=' + $scope.submitkey;
+        url = 'http://xxt.ctsi.com.cn/rest/app/enroll/record/submit?mpid=' + $scope.mpid + '&aid=' + $scope.aid + '&submitkey=' + $scope.submitkey;
         $http.post(url, posted).success(function(rsp) {
             if (typeof rsp === 'string') {
                 alert(rsp);
             } else if (rsp.err_code != 0) {
                 alert(rsp.err_msg);
             } else {
-                location.href = "http://180.153.55.67/views/default/cus/cctv/kzrl/today.html";
+                location.href = "http://xxt.ctsi.com.cn/views/default/cus/cctv/kzrl/today.html";
             }
         }).error(function(content, httpCode) {
             alert(content);
@@ -50,10 +50,10 @@ formApp.controller('formCtrl', ['$scope', '$http', '$timeout', '$q', function($s
         }, false);
         ele.click();
     };
-    $http.get('http://180.153.55.67/rest/app/enroll/record/submitkeyGet').success(function(rsp) {
+    $http.get('http://xxt.ctsi.com.cn/rest/app/enroll/record/submitkeyGet').success(function(rsp) {
         $scope.submitkey = rsp.data;
         r = new Resumable({
-            target: 'http://180.153.55.67/rest/app/enroll/record/uploadFile?mpid=' + $scope.mpid + '&aid=' + $scope.aid + '&submitkey=' + $scope.submitkey,
+            target: 'http://xxt.ctsi.com.cn/rest/app/enroll/record/uploadFile?mpid=' + $scope.mpid + '&aid=' + $scope.aid + '&submitkey=' + $scope.submitkey,
             testChunks: false,
             chunkSize: 256 * 1024,
             //simultaneousUploads: 1
@@ -78,11 +78,11 @@ formApp.controller('formCtrl', ['$scope', '$http', '$timeout', '$q', function($s
             }, 0);
         });
     });
-    $http.get('http://180.153.55.67/rest/cus/cctv/kzrl/score?id=1649&s=5').success(function(rsp) {
+    $http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/score?id=1649&s=5').success(function(rsp) {
         $scope.vcount = rsp.data[0];
         if (window.shaketv) {
             var sharelink, shareData;
-            sharelink = "http://180.153.55.67/rest/app/enroll";
+            sharelink = "http://xxt.ctsi.com.cn/rest/app/enroll";
             sharelink += "?mpid=" + $scope.mpid;
             sharelink += "&aid=" + $scope.aid;
             sharelink += "&shareby=";
@@ -90,13 +90,13 @@ formApp.controller('formCtrl', ['$scope', '$http', '$timeout', '$q', function($s
                 title: "我和" + $scope.vcount + "人在拍摄#我的胜利日#",
                 link: sharelink,
                 desc: '一起参加央视新闻#我的胜利日#视频拍摄，铭记历史，缅怀先烈。',
-                pic: 'http://180.153.55.67/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%88%91%E7%9A%84%E8%83%9C%E5%88%A9%E6%97%A5/62894570824539309.jpg'
+                pic: 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%88%91%E7%9A%84%E8%83%9C%E5%88%A9%E6%97%A5/62894570824539309.jpg'
             };
             window.shaketv.wxShare(shareData.pic, shareData.title, shareData.desc, shareData.link);
         }
     });
 }]);
-$('<img/>').attr('src', 'http://180.153.55.67/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
+$('<img/>').attr('src', 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
     $(this).remove();
-    $('body').css('background-image', 'url(http://180.153.55.67/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
+    $('body').css('background-image', 'url(http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
 });
