f37491992d38e8e8dd0fc46aa2eabc440c42420e
jasony62 <jasony62@qq.com>
2015-09-03 11:56:11 +0800

    93活动更新

--- views/default/cus/cctv/day93-yao/main.js
+++ views/default/cus/cctv/day93-yao/main.js
@@ -1,28 +1,25 @@
 formApp = angular.module('formApp', []);
 formApp.controller('formCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
-    $scope.goto = function(matter) {
-        location.href = matter.url;
+    $scope.goto = function(url) {
+        location.href = url;
     };
     $http.post('http://xxt.ctsi.com.cn/rest/mi/matter/logAccess?mpid=9f4335dc25ab0a83c04e066793cba286&id=kzrl-93&type=other&title=kzrl-93', {
         search: location.search.replace('?', ''),
         referer: document.referrer
     });
-    $http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/byChannel?mpid=9f4335dc25ab0a83c04e066793cba286&id=15&page=1&size=2').success(function(rsp) {
-        $scope.matters = rsp.data;
-    });
-    $http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/score?id=1649&s=1').success(function(rsp) {
-        $scope.vcount = rsp.data[0];
-        if (window.shaketv) {
-            var shareData;
-            shareData = {
-                title: "我是九三胜利日阅兵的第" + $scope.vcount + "位参与者，铭记历史才能烛照未来！",
-                link: location.href,
-                desc: "我的胜利日",
-                pic: 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%88%91%E7%9A%84%E8%83%9C%E5%88%A9%E6%97%A5/62894570824539309.jpg'
-            };
-            window.shaketv.wxShare(shareData.pic, shareData.title, shareData.desc, shareData.link);
-        }
-    });
+    //$http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/byChannel?mpid=9f4335dc25ab0a83c04e066793cba286&id=15&page=1&size=2').success(function(rsp) {
+    //    $scope.matters = rsp.data;
+    //});
+    if (window.shaketv) {
+        var shareData;
+        shareData = {
+            title: "我是九三胜利日阅兵的位参与者，铭记历史才能烛照未来！",
+            link: location.href,
+            desc: "我的胜利日",
+            pic: 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%88%91%E7%9A%84%E8%83%9C%E5%88%A9%E6%97%A5/62894570824539309.jpg'
+        };
+        window.shaketv.wxShare(shareData.pic, shareData.title, shareData.desc, shareData.link);
+    }
     var eSlider, oSlider, lastY;
     eSlider = document.querySelector('#slider');
     oSlider = new Hammer(eSlider);
