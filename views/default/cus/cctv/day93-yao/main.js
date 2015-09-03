1b13fbae2ec331ecb03fda3bfb194556a30a2d0b
jasony62 <jasony62@qq.com>
2015-09-03 09:29:24 +0800

    更新93代码

--- views/default/cus/cctv/day93-yao/main.js
+++ views/default/cus/cctv/day93-yao/main.js
@@ -1,8 +1,12 @@
 formApp = angular.module('formApp', []);
-formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
+formApp.controller('formCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
     $scope.goto = function(matter) {
         location.href = matter.url;
     };
+    $http.post('http://xxt.ctsi.com.cn/rest/mi/matter/logAccess?mpid=9f4335dc25ab0a83c04e066793cba286&id=kzrl-93&type=other&title=kzrl-93', {
+        search: location.search.replace('?', ''),
+        referer: document.referrer
+    });
     $http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/byChannel?mpid=9f4335dc25ab0a83c04e066793cba286&id=15&page=1&size=2').success(function(rsp) {
         $scope.matters = rsp.data;
     });
@@ -18,12 +22,6 @@ formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
             };
             window.shaketv.wxShare(shareData.pic, shareData.title, shareData.desc, shareData.link);
         }
-    });
-}]);
-(function() {
-    $('<img/>').attr('src', 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
-        $(this).remove();
-        $('body').css('background-image', 'url(http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
     });
     var eSlider, oSlider, lastY;
     eSlider = document.querySelector('#slider');
@@ -51,11 +49,17 @@ formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
                 eSlider.style.webkitTransform = transform;
             }
             lastY = 64;
-            setTimeout(function() {
-                location.href = "http://app.cntv.cn/special/dyb";
+            $timeout(function() {
+                location.href = "http://xxt.ctsi.com.cn/views/default/cus/cctv/kzrl/today.html";
             }, 10);
         } else {
             lastY = newY;
         }
+    });
+}]);
+(function() {
+    $('<img/>').attr('src', 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
+        $(this).remove();
+        $('body').css('background-image', 'url(http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
     });
 })();
--- views/default/cus/cctv/day93/main.js
+++ views/default/cus/cctv/day93/main.js
@@ -4,7 +4,7 @@ if (/MicroMessenger/i.test(navigator.userAgent)) {
     wx.config(signPackage);
 }
 formApp = angular.module('formApp', []);
-formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
+formApp.controller('formCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
     window.xxt.share.options.descAsTitle = true;
     window.xxt.share.options.logger = function(shareto) {
         var url = "/rest/mi/matter/logShare";
@@ -26,12 +26,6 @@ formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('http://xxt.ctsi.com.cn/rest/cus/cctv/kzrl/score?id=1649&s=1').success(function(rsp) {
         $scope.vcount = rsp.data[0];
         window.xxt.share.set("我的胜利日", location.href, "我是九三胜利日阅兵的第" + $scope.vcount + "位参与者，铭记历史才能烛照未来！", 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%88%91%E7%9A%84%E8%83%9C%E5%88%A9%E6%97%A5/62894570824539309.jpg');
-    });
-}]);
-(function() {
-    $('<img/>').attr('src', 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
-        $(this).remove();
-        $('body').css('background-image', 'url(http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
     });
     var eSlider, oSlider, lastY;
     eSlider = document.querySelector('#slider');
@@ -59,11 +53,22 @@ formApp.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
                 eSlider.style.webkitTransform = transform;
             }
             lastY = 64;
-            setTimeout(function() {
-                location.href = "http://app.cntv.cn/special/dyb";
+            $timeout(function() {
+                $http.post('/rest/mi/matter/logAccess?mpid=9f4335dc25ab0a83c04e066793cba286&id=kzrl-cntv&type=other&title=kzrl-cntv', {
+                    search: location.search.replace('?', ''),
+                    referer: document.referrer
+                }).success(function() {
+                    location.href = "http://app.cntv.cn/special/dyb";
+                });
             }, 10);
         } else {
             lastY = newY;
         }
+    });
+}]);
+(function() {
+    $('<img/>').attr('src', 'http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg').load(function() {
+        $(this).remove();
+        $('body').css('background-image', 'url(http://xxt.ctsi.com.cn/kcfinder/upload/ad483481fb907d53d74130cd88e11d86/%E5%9B%BE%E7%89%87/%E6%8A%97%E6%88%98%E6%97%A5%E5%8E%86/12/top.jpg)');
     });
 })();
